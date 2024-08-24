8cc8aea5243897e3fc82f37c5ecb4770f9df6e96
nndr229 <nithin.d@branchX.in>
2024-08-24 16:34:04 +0530

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("getmydata")]
+        public IActionResult Getmyinfo()
+        {
+            userdata _userdata =new userdata() { };
+            Console.WriteLine("this my data");
+            return Ok("the api is called");
+        }
+
+        public class userdata
+        {
+            public userdata() { 
+            
+            }
+        long id { get; set; }
+        string  name { get; set; }
+        string Dept { get; set; }
+        
+        }
+    }
+}
